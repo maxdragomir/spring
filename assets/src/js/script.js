5767d288ff81d5bcab6d5349fc4d9b764e7ae870
maxim1994 <rbcnjxrf146>
2018-08-15 21:05:31 +0300

    spring

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,20 +1,15 @@
-//copy
-function copy(element_id){
-    var aux = document.createElement("div");
-    aux.setAttribute("contentEditable", true);
-    aux.innerHTML = document.getElementById(element_id).innerHTML;
-    aux.setAttribute("onfocus", "document.execCommand('selectAll',false,null)");
-    document.body.appendChild(aux);
-    aux.focus();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-}
-
 
 $(document).ready(function(){
 
+    //hide modal
+    $('.hide-window').on('click', function () {
+        $('body').toggleClass('overflow-auto');
+        $(this).parents('.modal.open').toggleClass('modal-hide');
+        $(this).parents('.modal.open').next().toggleClass('new-overlay');
+    });
 
 
+    //under construction
     $('.under-construction-page').addClass('active');
     setTimeout(function () {
         $('.card-filters label').addClass('show-label');
@@ -28,8 +23,7 @@ $(document).ready(function(){
         $('.clear-btn').append(el);
         el.addClass('active');
 
-    });
-    $('.card-button').on('click', '.search-btn', function () {
+    }).on('click', '.search-btn', function () {
 
         var el = $('.search-btn i').clone().removeClass('active');
         $('.search-btn i').remove();
@@ -38,12 +32,8 @@ $(document).ready(function(){
 
     });
 
+    //log out
     $('.log-out').on('click', function (e) {
-        // var isYes = confirm("Are you sure?");
-        //
-        // if(!isYes) {
-        //     e.preventDefault();
-        // }
         e.preventDefault();
 
         swal({
@@ -54,10 +44,9 @@ $(document).ready(function(){
             confirmButtonClass: 'waves-effect',
             cancelButtonClass: 'waves-effect',
             showCancelButton: true,
-            reverseButtons: true
         }).then((result) => {
             if (result.value) {
-                location.href = '/login.html';
+                location.href = 'login.html';
             }
         })
 
@@ -85,9 +74,6 @@ $(document).ready(function(){
     //     }
     // }, 500);
 
-    // $('.show-mode').on('click', function () {
-    //     $('.mode').toggleClass('active').tooltip('close');
-    // });
 
     $('#list-products').addInputArea({
         area_var: '.products_var',
@@ -130,14 +116,44 @@ $(document).ready(function(){
         $('#content-block').addClass('active').slideDown();
     });
 
+
+    //modal close
+    $('.modal-ask-close').on('click', function () {
+        swal({
+            title: 'Are you sure?',
+            text: 'If you bla bla bla bla',
+            type: 'warning',
+            cancelButtonText: 'No',
+            confirmButtonText: 'Yes',
+            confirmButtonClass: 'waves-effect',
+            cancelButtonClass: 'waves-effect',
+            showCancelButton: true
+        }).then((result) => {
+            if (result.value) {
+                $('.modal-style').modal('close');
+                $('.modal-overlay').removeClass('new-overlay');
+            }
+        })
+    });
+
 //Materialize func ------------
-    $('.modal').modal();
+    $('.modal-style').modal({
+        dismissible: false,
+        onCloseEnd: function(){
+            $('body').removeClass('overflow-auto');
+            $('.modal-style').removeClass('modal-hide');
+        },
+        onCloseStart: function(){
+            $('.modal-overlay').removeClass('new-overlay');
+
+        }
+    });
 
     $('input[class*="timepicker-"]').timepicker({
         vibrate: true,
         twelveHour: false,
         autoClose: true,
-        // showClearBtn: true
+        showClearBtn: true
     });
 
     $('input[class*="datepicker-"]').datepicker({
@@ -147,7 +163,7 @@ $(document).ready(function(){
         selectMonths: true,
         autoClose: true,
         selectYears: 50,
-        // showClearBtn: true,
+        showClearBtn: true,
         format: 'dd/mmm/yyyy',
         onSelect: function(obj){
 
@@ -181,15 +197,6 @@ $(document).ready(function(){
         }
     });
 
-
-// Modes
-
-
-    $('.show-mode').on('click', function () {
-        $(this).children('.test-mode').toggleClass('active');
-        $(this).children('.live-mode').toggleClass('active');
-        $(this).nextAll('.text-status').toggleClass('active');
-    });
 
 });
 
